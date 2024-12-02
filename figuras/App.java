b384b7494041dab121790b15c3969052b9555cc8
Héctor <hectorlm203@gmail.com>
2024-12-02 20:46:50 +0100

    cambiado by por por

--- figuras/App.java
+++ figuras/App.java
@@ -7,7 +7,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int option;
         do {
-            System.out.println("Calculadora de Área by [Héctor]");
+            System.out.println("Calculadora de Área por [Héctor]");
             System.out.println("Selecciona una opción:");
             System.out.println("1. Círculo");
             System.out.println("2. Cuadrado");
